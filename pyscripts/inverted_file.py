ac85fdf616dc12aa1feb6ecee9bdc8cb24e98e3a
Nicolas <nicolas.six@insa-lyon.fr>
2017-10-13 16:56:55 +0200

    basic posting list read write into file

--- pyscripts/inverted_file.py
+++ pyscripts/inverted_file.py
@@ -23,7 +23,7 @@ class InvertedFile(object):
                   - date : string, when the article is written
                   - length : integer, how many words are in the article
                   - text : 2D list of string, where each list is paragraph, represented by a list of tokens
-          return :
+          - return:
               - integer, a score relative to a word and an article. Please note that the score is expected to be relative 
                 to a specific "word", not to "the word XXX at the position XXX". For example, in the article "The black hound ate the 
                 black bear.", there is a single, unique score relative to the word "black". 
@@ -38,15 +38,19 @@ class InvertedFile(object):
          more infos)
     
     Class Attributes :
+
         - score_len : integer, the max number of bytes allowed for the encoding of a score
         - id_len : integer, the max number of bytes allowed for the encoding of a docid
         - list_len_len : integer, the max number of bytes allowed for the encoding of the size of a value associated in __map (in bytes)
           Example : if list_len_len = 4, then the maximum size of a list is pow(2, 8*4) -1 bytes
+        - key_len_len : integer, the max number of bytes allowed for the encoding of the size of the key (in bytes)
+
     """
 
     score_len = 4
     id_len = 6
     list_len_len = 4
+    key_len_len = 1
 
     def __init__(self, score_function):
         self.__map = sd()
@@ -55,14 +59,13 @@ class InvertedFile(object):
     def add_document(self, document):
         """
         Add an article in the data structure.
-        parameters :
-            - document : dictionary, an element of the list <FormattedDocument.matches>, of shape (id, title, date, length, text) :
+        :param document : dictionary, an element of the list <FormattedDocument.matches>, of shape (id, title, date, length, text) :
                   - id : integer, the id of an article
                   - title : list of string, the title of the article tokenized
                   - date : string, when the article is written
                   - length : integer, how many words are in the article
                   - text : 2D list of string, where each list is paragraph, represented by a list of tokens
-        return : None
+        :return:None
         """
         paragraph_tokens = document["text"]
         paragraph_tokens.append(document["title"])
@@ -80,21 +83,40 @@ class InvertedFile(object):
         """
         TODO : Complete this method
         """
-        return self.__encode_posting_list(self.__map.keys()[0], self.__map[self.__map.keys()[0]])
+        output = self.__encode_posting_list(self.__map.keys()[0], self.__map[self.__map.keys()[0]])
+        with open(filename, 'wb+')as f:
+            f.write(output)
+
+    @classmethod
+    def __encode_number(cls, number, bin_size):
+        """
+
+        :param number:
+        :return:
+        """
+        if number > 2 ** (bin_size*8):
+            raise OutOfBoundError('Number is too long ({} > 2**({} * 8))'.format(number, bin_size))
+
+        offset_max = (bin_size -1) * 8
+        offset_actual = 0
+        n_list = []
+        while offset_actual <= offset_max:
+            tps = [(number >> offset_actual) & 255]
+            offset_actual += 8
+            n_list = tps + n_list
+        output = bytearray(n_list)
+        return output
 
     @classmethod
     def __encode_key(cls, key):
         """
         Encode a string in binary, in the format : <key_size(1 byte)><key(key_size bytes)>
-        parameters :
-            - key : string, the key to encode
-        return :
+        :param key : string, the key to encode
+        :return:
             - bytearray, the string encoded
         """
         output = bytearray(key, 'utf-8')
-        len_part = bytearray(chr(len(output)), 'utf-8')
-        if len(len_part) > 1:
-            raise OutOfBoundError('Word <{}> is too long ({} characters > 255)'.format(key, len(key)))
+        len_part = cls.__encode_number(len(output), cls.key_len_len)
         output = len_part + output
         return output
 
@@ -102,61 +124,38 @@ class InvertedFile(object):
     def __encode_score(cls, score):
         """
         Encode an integer in binary, in the format : <score(score_len bytes)>
-        parameters :
-            - score : integer, the number to encode
-        return :
+        :param score : integer, the number to encode
+        :return:
             - bytearray, the number encoded
         """
-        output = bytearray(chr(score), 'utf-8')
-        if len(output) > cls.score_len:
-            raise OutOfBoundError('Score is too high ({} > {}'.format(score, 256*cls.score_len - 1))
-        else:
-            while len(output) < cls.score_len:
-                output = b'\x00' + output
-
-        return output
+        return cls.__encode_number(score, cls.score_len)
 
     @classmethod
     def __encode_doc_id(cls, doc_id):
         """
         Encode an integer in binary, in the format : <id(id_len bytes)>
-        parameters :
-            - doc_id : integer, the number to encode
-        return :
+        :param doc_id : integer, the number to encode
+        :return:
             - bytearray, the number encoded
         """
-        output = bytearray(chr(doc_id), 'utf-8')
-        if len(output) > cls.id_len:
-            raise OutOfBoundError('Id is too high ({} > {}'.format(doc_id, 256 * cls.id_len - 1))
-        else:
-            while len(output) < cls.id_len:
-                output = b'\x00' + output
-
-        return output
+        return cls.__encode_number(doc_id, cls.id_len)
 
     @classmethod
     def __encode_list(cls, map_content):
         """
         Encode a list of (integer docid, integer score) in binary, in the format : 
         <list_len(list_len_len bytes)>( (<doc_id(id_len bytes)><score(score_len bytes)>)*N )
-        parameters :
-            - map_content : list, list of tuples (docid, score) where:
+        :param map_content : list, list of tuples (docid, score) where:
                 - docid : integer, id of an article
                 - score : integer, score of an article relative to some word
-        return :
+        :return:
             - bytearray, the list encoded
         """
         output = bytearray()
         for (doc_id, score) in map_content:
             output += cls.__encode_doc_id(doc_id)
             output += cls.__encode_score(score)
-        list_len = bytearray(chr(len(output)), 'utf-8')
-        if len(list_len) > cls.list_len_len:
-            raise OutOfBoundError('Posting list too long. Encoding takes {} bytes, '.format(len(output)) +\
-                                  'maximum allowed is {}'.format(256*cls.list_len_len-1))
-        else:
-            while len(list_len) < cls.list_len_len:
-                list_len = b'\x00' + list_len
+        list_len = cls.__encode_number(len(output), cls.list_len_len)
         output = list_len + output
         return output
 
@@ -165,12 +164,142 @@ class InvertedFile(object):
         """
         Encode a pair (key, value) in binary, in the format : 
         <key_size(1 byte)><key(key_size bytes)> <list_len(list_len_len bytes)>( (<doc_id(id_len bytes)><score(score_len bytes)>)*N )
-        parameters :
-            - key : string, a word, key of the map representing the index
-            - map_content : list, list of tuples (docid, score) where:
+        :param key : string, a word, key of the map representing the index
+        :param map_content : list, list of tuples (docid, score) where:
                 - docid : integer, id of an article
                 - score : integer, score of an article relative to some word
-        return :
+        :return:
             - bytearray, the pair encoded
         """
         return cls.__encode_key(key) + cls.__encode_list(map_content)
+
+    def decode_posting_lists(self, keys, filename):
+        """
+
+        :param keys:
+        :param bin_posting_list_list:
+        :return:
+        """
+        with open(filename, 'rb') as f:
+
+            while True:
+                # get key
+                bin_key_len = f.read(self.key_len_len)
+                if len(bin_key_len) != self.key_len_len:
+                    break
+                key_len = self.__decode_key_len(bin_key_len)
+
+                bin_key = f.read(key_len)
+                key = self.__decode_key(bin_key)
+
+                # get list len
+                bin_list_len = f.read(self.list_len_len)
+                list_len = self.__decode_list_len(bin_list_len)
+
+                # if key is one of the wanted keys
+                if key in keys:
+                    posting_list = self.__decode_list(f.read(list_len))
+                    self.__map[key] = posting_list
+                else:
+                    f.seek(list_len, 1)
+
+
+    @classmethod
+    def __decode_number(cls, bin_number):
+        """
+
+        :param bin_number:
+        :return:
+        """
+        int_val = 0
+        for octet in bin_number:
+            int_val <<= 8
+            int_val += octet
+        return int_val
+
+    @classmethod
+    def __decode_key(cls, bin_key):
+        """
+
+        :param bin_key:
+        :return:
+        """
+        return bin_key.decode('utf-8')
+
+    @classmethod
+    def __decode_key_len(cls, bin_key_len):
+        """
+
+        :param bin_key_len:
+        :return:
+        """
+        return cls.__decode_number(bin_key_len)
+
+    @classmethod
+    def __decode_list_len(cls, bin_list_len):
+        """
+
+        :param bin_list_len:
+        :return:
+        """
+        return cls.__decode_number(bin_list_len)
+
+    @classmethod
+    def __bin_article_regenerator(cls, bin_list, size_of_article):
+        """
+
+        :param bin_list:
+        :param size_of_article:
+        :return:
+        """
+        cur_index = 0
+        while cur_index + size_of_article <= len(bin_list):
+            yield bin_list[cur_index:cur_index+size_of_article]
+            cur_index += size_of_article
+        return
+
+    @classmethod
+    def __decode_list(cls, bin_list):
+        """
+        
+        :param bin_list: 
+        :return: 
+        """
+        output = []
+        article_gen = cls.__bin_article_regenerator(bin_list, cls.id_len + cls.score_len)
+        for bin_article in article_gen:
+            output.append(cls.__decode_article(bin_article))
+
+        return output
+
+
+    @classmethod
+    def __decode_article(cls, bin_article):
+        """
+
+        :param bin_article:
+        :return:
+        """
+        bin_doc_id = bin_article[:cls.id_len]
+        bin_score = bin_article[cls.id_len:]
+        doc_id = cls.__decode_number(bin_doc_id)
+        score = cls.__decode_number(bin_score)
+        return doc_id, score
+
+    @classmethod
+    def __decode_score(cls, bin_score):
+        """
+
+        :param bin_score:
+        :return:
+        """
+        return cls.__decode_number(bin_score)
+
+    @classmethod
+    def __decode_doc_id(cls, bin_doc_id):
+        """
+
+        :param bin_doc_id:
+        :return:
+        """
+        return cls.__decode_number(bin_doc_id)
