e1c8d555d1b7ef22851318c07f5f2ae082863d87
frobion <francois.robion@insa-lyon.fr>
2017-10-19 17:49:15 +0200

    Add script part to query.py and inverted_file.py so they can be executed standalone. Fix bug in fagin's algorithm

--- pyscripts/query.py
+++ pyscripts/query.py
@@ -2,11 +2,14 @@ import nltk
 
 
 class Query:
-    def __init__(self, query, tokenizer=nltk, conjunctive=True):
+    def __init__(self, query, tokenizer=nltk, filename="inverted_file.if", conjunctive=True):
         self._query_token_list = list(set(tokenizer.word_tokenize(query)))  # remove duplicate
+        if not self._query_token_list:
+            raise ValueError("A query must be non-empty")
         if not conjunctive:
             raise NotImplementedError("Disjunctive query not yet supported")
         self._conjunctive = conjunctive
+        self._filename = filename
 
     @staticmethod
     def _score_function(score_1, score_2):
@@ -14,14 +17,15 @@ class Query:
 
 
 class NaiveQuery(Query):
-    def __init__(self, query, tokenizer=nltk, conjunctive=True):
-        super().__init__(query, tokenizer, conjunctive)
+    def __init__(self, query, tokenizer=nltk, filename="inverted_file.if", conjunctive=True):
+        super().__init__(query, tokenizer, filename, conjunctive)
 
     def execute(self, inverted_file, top_k=5):
         # TODO: return a wrong result for the query "house divorce" on the document la123189
         if not self._query_token_list:
             return []
 
+        inverted_file.read_posting_lists(self._query_token_list, self._filename)
         result = inverted_file.map[self._query_token_list[0]]
         for token in self._query_token_list[1:]:
             result = self.__merge_posting_list(result, inverted_file.map[token])
@@ -54,13 +58,13 @@ class NaiveQuery(Query):
 
 
 class FaginQuery(Query):
-    def __init__(self, query, tokenizer=nltk, conjunctive=True):
-        super().__init__(query, tokenizer, conjunctive)
+    def __init__(self, query, tokenizer=nltk, filename="inverted_file.if", conjunctive=True):
+        super().__init__(query, tokenizer, filename, conjunctive)
 
     def execute(self, inverted_file, top_k=5):
-        # TODO: TOCHECK query with no text
-        # TODO: TOCHECK query with one token
         # In this method, pl stands for "posting_list"
+
+        inverted_file.read_posting_lists(self._query_token_list, self._filename)
 
         # The ith element of used_pl_sorted_by_score and used_pl_sorted_by_doc_id
         # correspond to the same document
@@ -76,8 +80,7 @@ class FaginQuery(Query):
         sorted_access_count = 0
         seen_documents = set()
 
-        # TODO: Update the loop to be able to break inside the for instead of waiting to the end of it
-        while len(current_best) < top_k and score_min < tau:
+        while len(current_best) < top_k or score_min < tau:
             for current_pl_index, index_in_current_pl in enumerate(index_table):
                 # Search for the first unseen document from the current posting list, and increment the
                 # current posting list's index in the index_table
@@ -101,25 +104,22 @@ class FaginQuery(Query):
                         other_document_score = self.__find_score_by_doc_id(other_pl, document)
                         current_document_score = self._score_function(current_document_score, other_document_score)
 
+                # If a word is not in the document, a negative score will be affected
                 if current_document_score < 0:
                     continue
-                # TODO: TOCHECK If |C|<k
+
                 if len(current_best) < top_k:
                     self.__reverse_insert(current_best, [document, current_document_score])
                     score_min = current_best[-1][1]
-                # TODO: TOCHECK Else if we have to replace the worst element of C
                 elif score_min < current_document_score:
                     current_best.pop()
                     self.__reverse_insert(current_best, [document, current_document_score])
                     score_min = current_best[-1][1]
-                # TODO: TOCHECK If at least one doc has been seen in sorted access for each posting list, update tau
                 if sorted_access_count >= len(index_table):
                     tau = 0
                     for tau_pl_index, index_in_tau_pl in enumerate(index_table):
                         _, tau_i = used_pl_sorted_by_score[tau_pl_index][index_in_tau_pl - 1]
                         tau += tau_i
-                        # TODO: TOCHECK Is parallel sorted access ok ?
-        # TODO: TOCHECK Return
         return current_best
 
     @staticmethod
@@ -159,82 +159,16 @@ class FaginQuery(Query):
         list_.insert(min_index, x)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     from pyscripts.inverted_file import InvertedFile
-    from pyscripts.formatted_document import FormattedDocument
     from pyscripts.tokenizer import Tokenizer
-    from pyscripts.query import NaiveQuery
-    from pyscripts.query import FaginQuery
-    import glob
-    import xml.etree.ElementTree as ET
-    from xml.etree.ElementTree import ParseError
 
-
-    def read_files(paths, n=-1):
-        """
-        Read n files from a list of paths and convert them as xml trees. A root node <RAC> is added to every file to avoid some
-        ParseError
-        parameters :
-            - paths : enumeration of strings, a list of absolute paths where datas have to be read (datas must be xml files)
-            - n : number of files needed to be read, if -1, every possible files will be read
-        return :
-            - a list of len=(min(n, number of files) if n != -1, else number of files) of xml trees representations
-              of the documents
-        """
-        output = []
-        for path in paths:
-            try:
-                txt = open(path, 'r').read()
-                output.append(ET.fromstring('<RAC>' + txt + '</RAC>'))
-                n -= 1
-                print('Successfully parsed document <{}>'.format(path))
-            except ParseError as e:
-                print('Can\'t parse document <{}>. Doesn\'t matter, skip'.format(path))
-            except IsADirectoryError:
-                print('Can\'t parse directory <{}>. Doesn\'t matter, skip'.format(path))
-            if n == 0:
-                return output
-        return output
-
-
-    def score(token, document):
-        paragraph_tokens = document['text'].copy()
-        paragraph_tokens.append(document['title'])
-        token_count = 0
-        for paragraph in paragraph_tokens:
-            for word in paragraph:
-                if word == token:
-                    token_count += 1
-        return token_count
-
-
-    inverted_file = InvertedFile(score)
-
-    LATIMES_PATH = './latimes'
-    files = glob.iglob(LATIMES_PATH + '/*')
-    xml_files = read_files(files, 1)
-    fd = FormattedDocument(xml_root_doc=xml_files[0], tokenizer=Tokenizer())
-
-    # add the article of the loaded document to the inferted file
-    for doc in fd.matches:
-        inverted_file.add_document(doc)
-
-
-    def sort_by_score(posting_list):
-        return sorted(posting_list, key=lambda x: x[1], reverse=True)
-
-
-    # print("\nPL sorted by doc id :")
-    # house_pl = inverted_file.map["hous"]
-    # divorce_pl = inverted_file.map["divorc"]
-    # print(house_pl, "\n\n", divorce_pl)
-    # print("\n\nPL sorted by score :")
-    # print(sort_by_score(house_pl), "\n\n", sort_by_score(divorce_pl))
-
+    top_k = 8
+    inverted_file = InvertedFile(None)
     print("Create and execute fagin query")
-    query = FaginQuery("the, it", Tokenizer())
-    print(query.execute(inverted_file, 6))
+    query = FaginQuery("the horse in the field", Tokenizer())
+    print(query.execute(inverted_file, top_k))
 
     print("Create and execute naive query")
     naive_query = NaiveQuery("the, it", Tokenizer())
-    print(naive_query.execute(inverted_file, 6))
+    print(naive_query.execute(inverted_file, top_k))
